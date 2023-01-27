85c22179ffdcd6a8d81cdb58d1d2a7e1b7cf7c54
JoseADAW <1898288@alu.murciaeduca.es>
2023-01-27 12:26:16 +0100

    Fin paso OBS

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,5 +1,10 @@
-<header>
-    <div class="container">
-        Hola Mundo
+<header class="bg-trueGray-700">
+    <div class="container flex items-center h-16">
+        <a>
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4
+12h16M4 18h16" />
+            </svg>
+        </a>
     </div>
 </header>
